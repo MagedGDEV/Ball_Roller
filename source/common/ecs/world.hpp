4bf685227f5ec56c3cacada6f2d7a0c023c5d33d
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2022-12-11 00:31:40 +0200

    initial implementation of ecs (not tested)

--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -26,6 +26,13 @@ namespace our {
         Entity* add() {
             //TODO: (Req 8) Create a new entity, set its world member variable to this,
             // and don't forget to insert it in the suitable container.
+
+            // Create a new entity
+            Entity* entity = new Entity();
+            // Set its world member variable to this
+            entity->world = this;
+            // Insert it in the entities container
+            entities.insert(entity);
             return nullptr;
         }
 
@@ -38,17 +45,34 @@ namespace our {
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
         void markForRemoval(Entity* entity){
             //TODO: (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
+            
+            if (entities.find(entity) != entities.end()) {
+                markedForRemoval.insert(entity);
+            }
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
         void deleteMarkedEntities(){
             //TODO: (Req 8) Remove and delete all the entities that have been marked for removal
+
+            for (auto entity : markedForRemoval) {
+                entities.erase(entity);
+                delete entity;
+            }
+            markedForRemoval.clear();
         }
 
         //This deletes all entities in the world
         void clear(){
             //TODO: (Req 8) Delete all the entites and make sure that the containers are empty
+
+            for (auto entity : entities) {
+                entities.erase(entity);
+                delete entity;
+            }
+            entities.clear();
+            markedForRemoval.clear();
         }
 
         //Since the world owns all of its entities, they should be deleted alongside it.
