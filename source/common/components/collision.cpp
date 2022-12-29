50e229ecea1a54a287d5bd326533b9b6c1f7bf13
Yomna Ragab <62198548+YomnaTarek@users.noreply.github.com>
2022-12-29 17:02:32 +0200

    Added comments

--- source/common/components/collision.cpp
+++ source/common/components/collision.cpp
@@ -3,7 +3,7 @@
 #include "../deserialize-utils.hpp"
 
 namespace our {
-    // Reads linearVelocity & angularVelocity from the given json object
+    // Reads collision parameters from the given json object
     void CollisionComponent::deserialize(const nlohmann::json& data){
         if(!data.is_object()) return;
         radius = data["radius"].get<float>();
