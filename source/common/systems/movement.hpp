07e4cde73b84b6c831462090da5435ebaca81d9b
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2022-12-30 20:30:10 +0200

    fixed crashing

--- source/common/systems/movement.hpp
+++ source/common/systems/movement.hpp
@@ -20,23 +20,20 @@ namespace our
         // This should be called every frame to update all entities containing a MovementComponent. 
         void update(World* world, float deltaTime) {
             // For each entity in the world
+            if (world == nullptr) return;
             for(auto entity : world->getEntities()){
                 // Get the movement component if it exists
                 MovementComponent* movement = entity->getComponent<MovementComponent>();
                 // If the movement component exists
                 if(movement){
                     //Check if the entity is colliding with another entity
-                    if (collisionSystem->CheckCollision(world, entity))
-                    {
-                        //delete the other marked entity from the world
-                        world->deleteMarkedEntities();
-                        
-                    }
+                    collisionSystem->CheckCollision(world, entity);
                     // Change the position and rotation based on the linear & angular velocity and delta time.
                     entity->localTransform.position += deltaTime * movement->linearVelocity;
                     entity->localTransform.rotation += deltaTime * movement->angularVelocity;
                 }
             }
+            world->deleteMarkedEntities();
         }
 
     };
